00252f9e17966c3831fe2d22afb06bc6591767fb
iostream <daniel5555@gmail.com>
2013-07-27 02:05:16 +0200

    Fixing bugs.

--- sys/vfs/hammer2/hammer2.h
+++ sys/vfs/hammer2/hammer2.h
@@ -79,14 +79,14 @@ struct hammer2_span;
 struct hammer2_state;
 struct hammer2_msg;
 
-extern static struct objcache *cache_buffer_read;
-extern static struct objcache *cache_buffer_write;
-
 MALLOC_DECLARE(C_BUFFER);
 MALLOC_DEFINE(C_BUFFER, "compbuffer", "Buffer used for compression.");
 
 MALLOC_DECLARE(D_BUFFER);
 MALLOC_DEFINE(D_BUFFER, "decompbuffer", "Buffer used for decompression.");
+
+extern static struct objcache *cache_buffer_read;
+extern static struct objcache *cache_buffer_write;
 
 /*
  * The chain structure tracks blockref recursions all the way to the root
