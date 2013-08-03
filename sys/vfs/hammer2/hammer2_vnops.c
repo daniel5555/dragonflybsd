a75748ed59204930117a1b8ce9183de0f09ca5b6
iostream <daniel5555@gmail.com>
2013-08-03 05:19:43 +0200

    Starting with appropriate zero-checking.

--- sys/vfs/hammer2/hammer2_vnops.c
+++ sys/vfs/hammer2/hammer2_vnops.c
@@ -357,6 +357,7 @@ hammer2_zero_check_and_write(struct buf *bp, hammer2_trans_t *trans,
 		hammer2_chain_lookup_done(parent);
 		if (chain) {
 			kprintf("WRITE PATH: Found a chain.\n");
+			hammer2_chain_unlock(chain);
 			hammer2_chain_delete(trans, chain);
 			kprintf("WRITE PATH: Deleted a chain.\n");
 		}
