b6008af3bfa0d40ba2d1f937bed8e54fc797e4f8
iostream <daniel5555@gmail.com>
2013-08-16 03:15:45 +0200

    Adding a lot of changes.

--- sys/vfs/hammer2/hammer2_vfsops.c
+++ sys/vfs/hammer2/hammer2_vfsops.c
@@ -661,7 +661,7 @@ hammer2_vfs_mount(struct mount *mp, char *path, caddr_t data,
 	hammer2_vfs_statfs(mp, &mp->mnt_stat, cred);
 	
 	mtx_init(&hmp->wthread_mtx);
-	bioq_init(hmp->bioq_write);
+	bioq_init(&hmp->wthread_bioq);
 	
 	/*
 	 * Launch threads.
@@ -698,17 +698,13 @@ hammer2_write_thread(void *arg)
 	mtx_lock(&hmp->wthread_mtx);
 	while (hmp->wthread_destroy == 0) {
 		kprintf("Executing write thread: outer thread.\n");
-		if (bioq_first(&hmp->whtread_bioq) == NULL) {
+		if (bioq_first(&hmp->wthread_bioq) == NULL) {
 			mtxsleep(&hmp->wthread_bioq, &hmp->wthread_mtx,
 				 0, "h2-write-thread-sleep", 0);
 		}
-		while ((bio = bioq_takefirst(&hmp->wthread_bioq)) != NULL) {
+		while ((bio = bioq_takefirst(hmp->wthread_bioq)) != NULL) {
 			kprintf("Executing write thread: inner thread.\n");
 			mtx_unlock(&hmp->wthread_mtx);
-			bio = bioq_takefirst(bioq_write);
-			--write;
-			mtx_unlock(thread_protect);
-			kprintf("Write thread: write = %d.\n", write);
 			
 			error = 0;
 			bp = bio->bio_buf;
@@ -1386,12 +1382,12 @@ hammer2_vfs_unmount(struct mount *mp, int mntflags)
 	hmp->wthread_destroy = 1;
 	wakeup(&hmp->wthread_destroy);
 	
-	mtxlock(&hmp->wthread_mtx);
+	mtx_lock(&hmp->wthread_mtx);
 	while (hmp->wthread_destroy != -1) {
 		mtxsleep(&hmp->wthread_destroy, &hmp->wthread_mtx, 0,
 		"umount-sleep",	0);
 	}
-	mtxunlock(&hmp->wthread_mtx);
+	mtx_unlock(&hmp->wthread_mtx);
 	//wakeup(&destroy);
 	
 	//kfree(bioq_write, W_BIOQUEUE);
