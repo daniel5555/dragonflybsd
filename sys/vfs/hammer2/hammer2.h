1ce17822eb2e2ce715164d0107c8c0c5c9b62416
iostream <daniel5555@gmail.com>
2013-08-16 04:02:09 +0200

    Adding a lot of changes.

--- sys/vfs/hammer2/hammer2.h
+++ sys/vfs/hammer2/hammer2.h
@@ -413,8 +413,8 @@ struct hammer2_mount {
 	int		volhdrno;	/* last volhdrno written */
 	hammer2_volume_data_t voldata;
 	hammer2_volume_data_t volsync;	/* synchronized voldata */
-	struct mtx wthread_mtx;     /* mutex for write thread */
 	struct bio_queue_head *wthread_bioq; /* bio queue for write thread */
+	struct mtx_t* wthread_mtx;     /* mutex for write thread */
 	int 	wthread_destroy;    /* to control the write thread */
 };
 
--- sys/vfs/hammer2/hammer2_vfsops.c
+++ sys/vfs/hammer2/hammer2_vfsops.c
@@ -660,9 +660,9 @@ hammer2_vfs_mount(struct mount *mp, char *path, caddr_t data,
 	 */
 	hammer2_vfs_statfs(mp, &mp->mnt_stat, cred);
 	
+	hmp->wthread_mtx_t = kmalloc(sizeof(mtx_t), W_MTX, M_INTWAIT);
 	
-	
-	mtx_init(&hmp->wthread_mtx);
+	mtx_init(hmp->wthread_mtx);
 	bioq_init(hmp->wthread_bioq);
 	hmp->wthread_destroy = 0;
 	
