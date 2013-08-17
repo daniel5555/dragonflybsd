e6d33acfc6b49667ab55b450073dfd11cce391ad
iostream <daniel5555@gmail.com>
2013-08-17 23:59:03 +0200

    hammer2 utulity minor changes.

--- sbin/hammer2/hammer2.h
+++ sbin/hammer2/hammer2.h
@@ -136,6 +136,9 @@ int cmd_show(const char *devpath, int dofreemap);
 int cmd_rsainit(const char *dir_path);
 int cmd_rsaenc(const char **keys, int nkeys);
 int cmd_rsadec(const char **keys, int nkeys);
+int cmd_setcomp(char* comp_string, char* file_string);
+int cmd_setcomp_recursive(char* option_string, char* comp_string, char* file_string);
+int cmd_setcomp_recursive_call(char *directory, int comp_method, int set_files);
 
 /*
  * Misc functions
@@ -151,6 +154,3 @@ uint32_t hammer2_icrc32(const void *buf, size_t size);
 uint32_t hammer2_icrc32c(const void *buf, size_t size, uint32_t crc);
 
 void hammer2_shell_parse(dmsg_msg_t *msg);
-int setcomp(char* comp_string, char* file_string);
-int setcomp_recursive(char* option_string, char* comp_string, char* file_string);
-int setcomp_recursive_call(char *directory, int comp_method, int set_files);
