9c91add94aa980ec38c94cddca9ff4eebec36d9f
iostream <daniel5555@gmail.com>
2013-08-23 22:20:46 +0200

    Adding zlib compression.

--- sys/vfs/hammer2/zlib/inflate.c
+++ sys/vfs/hammer2/zlib/inflate.c
@@ -206,12 +206,12 @@ int stream_size;
         strm->opaque = (voidpf)0;
 //#endif
     //}
-    if (strm->zfree == (free_func)0)
-#ifdef Z_SOLO
-        return Z_STREAM_ERROR;
-#else
-        strm->zfree = zcfree;
-#endif
+    //if (strm->zfree == (free_func)0)
+//#ifdef Z_SOLO
+        //return Z_STREAM_ERROR;
+//#else
+        //strm->zfree = zcfree;
+//#endif
     state = (struct inflate_state FAR *) kmalloc(sizeof(struct inflate_state), C_ZLIB_BUFFER_INFLATE, M_INTWAIT);
             /*ZALLOC(strm, 1, sizeof(struct inflate_state));*/
     if (state == Z_NULL) return Z_MEM_ERROR;
@@ -1448,47 +1448,47 @@ int ZEXPORT inflateCopy(dest, source)
 z_streamp dest;
 z_streamp source;
 {
-    struct inflate_state FAR *state;
-    struct inflate_state FAR *copy;
-    unsigned char FAR *window;
-    unsigned wsize;
+    //struct inflate_state FAR *state;
+    //struct inflate_state FAR *copy;
+    //unsigned char FAR *window;
+    //unsigned wsize;
 
-    /* check input */
-    if (dest == Z_NULL || source == Z_NULL || source->state == Z_NULL ||
-        source->zalloc == (alloc_func)0 || source->zfree == (free_func)0)
-        return Z_STREAM_ERROR;
-    state = (struct inflate_state FAR *)source->state;
+    ///* check input */
+    //if (dest == Z_NULL || source == Z_NULL || source->state == Z_NULL ||
+        //source->zalloc == (alloc_func)0 || source->zfree == (free_func)0)
+        //return Z_STREAM_ERROR;
+    //state = (struct inflate_state FAR *)source->state;
 
-    /* allocate space */
-    //copy = (struct inflate_state FAR *)
-           //ZALLOC(source, 1, sizeof(struct inflate_state));
-    if (copy == Z_NULL) return Z_MEM_ERROR;
-    window = Z_NULL;
-    if (state->window != Z_NULL) {
-        //window = (unsigned char FAR *)
-                 //ZALLOC(source, 1U << state->wbits, sizeof(unsigned char));
-        if (window == Z_NULL) {
-			kfree(copy, C_ZLIB_BUFFER_INFLATE);
-            //ZFREE(source, copy);
-            return Z_MEM_ERROR;
-        }
-    }
+    ///* allocate space */
+    ////copy = (struct inflate_state FAR *)
+           ////ZALLOC(source, 1, sizeof(struct inflate_state));
+    //if (copy == Z_NULL) return Z_MEM_ERROR;
+    //window = Z_NULL;
+    //if (state->window != Z_NULL) {
+        ////window = (unsigned char FAR *)
+                 ////ZALLOC(source, 1U << state->wbits, sizeof(unsigned char));
+        //if (window == Z_NULL) {
+			//kfree(copy, C_ZLIB_BUFFER_INFLATE);
+            ////ZFREE(source, copy);
+            //return Z_MEM_ERROR;
+        //}
+    //}
 
-    /* copy state */
-    zmemcpy((voidpf)dest, (voidpf)source, sizeof(z_stream));
-    zmemcpy((voidpf)copy, (voidpf)state, sizeof(struct inflate_state));
-    if (state->lencode >= state->codes &&
-        state->lencode <= state->codes + ENOUGH - 1) {
-        copy->lencode = copy->codes + (state->lencode - state->codes);
-        copy->distcode = copy->codes + (state->distcode - state->codes);
-    }
-    copy->next = copy->codes + (state->next - state->codes);
-    if (window != Z_NULL) {
-        wsize = 1U << state->wbits;
-        zmemcpy(window, state->window, wsize);
-    }
-    copy->window = window;
-    dest->state = (struct internal_state FAR *)copy;
+    ///* copy state */
+    //zmemcpy((voidpf)dest, (voidpf)source, sizeof(z_stream));
+    //zmemcpy((voidpf)copy, (voidpf)state, sizeof(struct inflate_state));
+    //if (state->lencode >= state->codes &&
+        //state->lencode <= state->codes + ENOUGH - 1) {
+        //copy->lencode = copy->codes + (state->lencode - state->codes);
+        //copy->distcode = copy->codes + (state->distcode - state->codes);
+    //}
+    //copy->next = copy->codes + (state->next - state->codes);
+    //if (window != Z_NULL) {
+        //wsize = 1U << state->wbits;
+        //zmemcpy(window, state->window, wsize);
+    //}
+    //copy->window = window;
+    //dest->state = (struct internal_state FAR *)copy;
     return Z_OK;
 }
 
