968313befc2768cd71ca6c8a3a30218df32690d8
iostream <daniel5555@gmail.com>
2013-08-23 04:56:40 +0200

    Adding zlib compression.

--- sys/vfs/hammer2/zlib/inflate.c
+++ sys/vfs/hammer2/zlib/inflate.c
@@ -85,6 +85,11 @@
 #include "inflate.h"
 #include "inffast.h"
 #include "hammer2.h"
+#include <sys/malloc.h> //for malloc macros
+
+MALLOC_DECLARE(C_ZLIB_BUFFER_INFLATE);
+MALLOC_DEFINE(C_ZLIB_BUFFER, "compzlibbufferinflate",
+	"A private buffer used by zlib library for inflate function.");
 
 #ifdef MAKEFIXED
 #  ifndef BUILDFIXED
