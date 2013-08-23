7e9b2bda62f8e5c389d81c129c3b716484157720
iostream <daniel5555@gmail.com>
2013-08-23 23:17:54 +0200

    Adding zlib compression.

--- sys/vfs/hammer2/zlib/zutil.h
+++ sys/vfs/hammer2/zlib/zutil.h
@@ -14,7 +14,7 @@
 #define ZUTIL_H
 
 #ifdef HAVE_HIDDEN
-#  define ZLIB_INTERNAL __attribute__((visibility ("hidden")))
+#  define ZLIB_INTERNAL __attribute__(visibility ("hidden"))
 #else
 #  define ZLIB_INTERNAL
 #endif
